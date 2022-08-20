939ddfeca61599d0586bffb327f92a11b49bd637
Idan Arye <IdanArye@gmail.com>
2022-08-20 18:32:12 +0300

    Just some test changes

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(SpriteBundle {
         texture: asset_server.load("icon.png"),
+        transform: Transform::from_xyz(0.0, -150.0, 0.0),
+        ..Default::default()
+    });
+
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        transform: Transform::from_xyz(0.0, 150.0, 0.0),
         ..Default::default()
     });
 }
